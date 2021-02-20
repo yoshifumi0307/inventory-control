ed5ba98659e67976b76b9bdb526f0c7b64b956c1
Yoshifumi Nishimura <nicchi0307@gmail.com>
2021-02-20 16:36:06 +0900

    detail changed user-control to inventory-control

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,4 @@ class User < ApplicationRecord
     validates :name, presence: true, length: {maximum: 100}
     validates :address, length: {maximum: 255}
     validates :phone, allow_blank: true, length: {maximum: 15}, numericality: true
-    validates :mail_address, length: {maximum: 255}, uniqueness: { case_sensitive: false }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
