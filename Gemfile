0091c414585b975772c3640fa122e4b0e7615918
Yoshifumi Nishimura <nicchi0307@gmail.com>
2021-03-03 14:34:17 +0900

    signup,login page created

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'rails-i18n', '~> 6.0.0' # For 6.0.0 or higher
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
+gem 'bcrypt'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -29,4 +29,13 @@ ActiveRecord::Schema.define(version: 2021_02_26_063446) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
 end
