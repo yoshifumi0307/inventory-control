da54ee709105036e1ca8ed676631f1a2427c77a0
Yoshifumi Nishimura <nicchi0307@gmail.com>
2021-03-09 14:59:02 +0900

    devise追加したいけど、出来ない

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'users/new'
+  # get 'sessions/new'
+  # get 'users/new'
   root 'top#home'
-  get '/signup', to: 'users#new'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get 'top/home'
+  # get '/signup', to: 'users#new'
+  # get    '/login',   to: 'sessions#new'
+  # post   '/login',   to: 'sessions#create'
+  # delete '/logout',  to: 'sessions#destroy'
   resources :users, :enterprises, :products
 end
