93f6261d54fd1c351385b19c0533be39c4b81bb4
Yoshifumi Nishimura <nicchi0307@gmail.com>
2021-03-13 16:23:31 +0900

    signup外装

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,13 +3,4 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
-  # ユーザーがログインしていればtrue、その他ならfalseを返す
-  def logged_in?
-    !current_user.nil?
-  end
-
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
-  end
 end
